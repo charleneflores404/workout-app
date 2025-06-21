66c104e1bc93a6dfb661430501436605eee7a180
Charlene <charlene.flores.404@gmail.com>
2025-06-21 11:39:14 +0800

    Format date

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,5 +1,7 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext();
 
@@ -26,7 +28,10 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Reps (kg): </strong>
         {workout.reps}
       </p>
-      <p>{workout.createdAt}</p>
+      {/* format date */}
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
       <span onClick={handleClick}>delete</span>
     </div>
   );
