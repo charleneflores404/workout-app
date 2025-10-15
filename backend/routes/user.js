8b0381d154c45ff2286de6d84dd65db4bdfd269a
Charlene <charlene.flores.404@gmail.com>
2025-10-15 23:01:39 +0800

    Use user controller functions in user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+// controller functions
+const { loginUser, signupUser } = require('../controllers/userController');
+
 const router = express.Router();
 
 // login
-router.post('/login', () => {});
+router.post('/login', loginUser);
 
 // signup
-router.post('/signup', () => {});
+router.post('/signup', signupUser);
 
 module.exports = router;
