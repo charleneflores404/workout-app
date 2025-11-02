7b86f7ef57ad95f9c55cde060e2d87259a6d97b1
Charlene <charlene.flores.404@gmail.com>
2025-11-02 23:25:45 +0800

    Prevent flashing workouts of previously logged in user to current user

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,13 +1,16 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: dispatchWorkouts } = useWorkoutsContext();
 
   const logout = () => {
     // remove the user from local storage
     localStorage.removeItem("user");
 
     // dispatch the logout action
+    dispatchWorkouts({ type: "SET_WORKOUTS", payload: null });
     dispatch({ type: "LOGOUT" });
   };
 
