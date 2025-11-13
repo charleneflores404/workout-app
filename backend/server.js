2e2f454184340c3bf23432509b71922ff58eb28f
Charlene <charlene.flores.404@gmail.com>
2025-11-14 00:06:07 +0800

    Configure CORS middleware to allow localhost and Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+require('cors');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 const userRoutes = require('./routes/user');
@@ -11,6 +12,29 @@ const app = express();
 // middleware to be able to access a request that has a BODY (data)
 // such as POST and PATCH
 app.use(express.json());
+
+// CORS setup
+const allowedOrigins = [
+  'http://localhost:3000', // dev frontend
+  'https://workout-app-proj.vercel.app', // production frontend
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin like Postman or server-to-server
+      if (!origin) return callback(null, true);
+      // if origin matched, allow the request and set the Access-Control-Allow-Origin header
+      // otherwise reject
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('CORS not allowed for this origin'));
+      }
+    },
+    // credentials: true // if using cookies or sessions
+  })
+);
 
 // // middleware to log reqs
 // app.use((req, res, next) => {
